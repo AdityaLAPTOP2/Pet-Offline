3c466dd7e449320c569996e80774a098cf03c707
AdityaLAPTOP2 <69348979+AdityaLAPTOP2@users.noreply.github.com>
2020-10-19 08:45:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var dog
+var game=1
+var car
+var boneImg
+var full
+var empty
+var feed 
+var happy
+var count = 26
+var happiness = 100
+function preload(){
+  happy=loadImage("images/Happy.png")
+  boneImg = loadImage("images/Bone.png");
+   car=loadImage("images/dogs.png");
+ //  milks = loadImage("images/milk.jpg")
+
+}
+
+
+function setup() {
+feed = createSprite(100,100);
+  createCanvas(800,400);
+ dog = createSprite(700,270)
+ dog.addImage(car)
+ dog.scale = 0.3
+ feed.addImage(boneImg)
+ feed.scale=0.1
+
+
+}
+
+function draw() {
+  /*if(keyCode===87&&count>0){
+    feed = createSprite(100,100);
+    feed.addImage(boneImg)
+    feed.scale=0.1
+
+  }*/
+if(game===1&&keyCode===32&&count>0){
+  feed.x=World.mouseX
+  feed.y=World.mouseY
+
+}
+if(feed.isTouching(dog)){
+game=2
+happiness=happiness+Math.round(random(1,7))
+  feed.destroy();
+  dog.addImage(happy);
+ 
+}
+if(keyCode===87&&game===2){
+   
+  count=count-1
+  game=1
+  feed = createSprite(100,100);
+  feed.addImage(boneImg)
+  feed.scale=0.1
+}
+
+if(World.frameCount%1800===0){
+  count=count+1
+}
+if(World.frameCount%180===0){
+  happiness=happiness-Math.round(random(0,3))
+}
+  background(255,255,255);  
+  drawSprites();
+  text("Food Left :"+count,100,50)
+  text("Dog happiness :"+happiness,200,50)
+
+}
